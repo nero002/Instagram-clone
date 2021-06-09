258b384d8d349497e2f54f2dcc58c36a88b317e2
Dheeraj Gupta <38027375+nero002@users.noreply.github.com>
2021-06-09 12:12:03 +0530

    1st

---
+++ app/src/main/java/com/nero/instagram_clone/MainScreen.kt
@@ -0,0 +1,35 @@
+package com.nero.instagram_clone
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.nero.instagram_clone.databinding.MainscreenBinding
+
+class MainScreen : AppCompatActivity() {
+
+    private lateinit var binding: MainscreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = MainscreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_mainscreen)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
